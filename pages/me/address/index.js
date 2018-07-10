62db662002a6421bee86d681240a58fd585ddc20
zoye1988 <nadt1988@126.com>
2018-07-10 12:25:20 +0800

    dhj

--- pages/me/address/index.js
+++ pages/me/address/index.js
@@ -1,18 +1,21 @@
 // pages/me/address/index.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    css: app.globalData.css,
+    AD: app.globalData.AD,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this;
+    app.setCssStyle();
   },
 
   /**
@@ -35,32 +38,4 @@ Page({
   onHide: function () {
   
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
