930d04efa51f47cc6f4af7f961ba89f5a6a32202
zoye1988 <nadt1988@126.com>
2018-07-15 12:05:38 +0800

    dhj

--- pages/me/bill/list/index.js
+++ pages/me/bill/list/index.js
@@ -6,9 +6,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-    currentTab:0,
+    currentTab: "all",
     css: app.globalData.css,
     AD: app.globalData.AD,
+    bills: [
+      {
+        bid: 1,
+        bcode: "CGT011023123",
+        price: 450.00,
+        time: "2018-01-02 09:21:21",
+        status: "pay",
+      },
+      {
+        bid: 2,
+        bcode: "CGT011023123",
+        price: 450.00,
+        time: "2018-01-02 09:21:21",
+        status: "ready",
+      },
+      {
+        bid: 3,
+        bcode: "CGT011023123",
+        price: 450.00,
+        time: "2018-01-02 09:21:21",
+        status: "receive",
+      },
+      {
+        bid: 4,
+        bcode: "CGT011023123",
+        price: 450.00,
+        time: "2018-01-02 09:21:21",
+        status: "delay",
+      }
+    ]
+  },
+
+  swichNav: function (e) {
+    var _current = e.currentTarget.dataset.current;
+    this.setData({
+      currentTab: _current
+    })
   },
 
   /**
@@ -23,46 +60,46 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /*
   swichNav
   */
-  
+
 })
