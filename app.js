0c77e4e916b3a39fcb5dca81211491b296ddfafc
zoye1988 <nadt1988@126.com>
2018-09-02 22:28:07 +0800

    dhj

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 //app.js
 App({
-  onLaunch: function () {
-    
-  },
   globalData: {
     ustatus: null,
-    css:"moon",
-    AD:true,     //广告位
+    css:"common",
+    AD:1,     //广告位
     ad_package:"满200包邮",
     ad_minus:"满100减10",
     user:{
@@ -14,35 +11,83 @@ App({
       uimg:"",
       openid:"",
       tel:"",
-      role:"customer",
-    }
+      role_EN:"customer",
+      role_CN:"普通用户",
+      /*
+      产品代理
+      店小二
+      */
+    },
+    /**
+     * 系统默认参数
+     */
+    downloadurl: 'http://192.168.3.69:8080/wxadmin/res/',//默认系统下载链接
+    host: "http://192.168.3.69:8080/wxadmin/",//默认系统数据访问地址
+    //downloadurl: 'http://192.168.31.194:8080/wxadmin/res/',//默认系统下载链接
+    //host: "http://192.168.31.194:8080/wxadmin/",//默认系统数据访问地址
 
   },
+
   onShow:function(){
-    this.setCssStyle();
+    console.log("onLoad");
+    console.log(this.globalData.css);
   },
 
   //登录时，全局读取用户信息
   onLaunch:function(){
-    var ustatus = wx.getStorageSync("ustatus");
-    console.log("ustatus="+ustatus);
-    if (ustatus==null || ustatus==""){
-      this.getLogin();
-    }else{
-      this.globalData.user.openid = wx.getStorageSync("user.openid");
-      this.globalData.user.uname = wx.getStorageSync("user.uname");
-      this.globalData.user.uimg = wx.getStorageSync("user.uimg");
-      this.globalData.user.tel = wx.getStorageSync("user.tel");
-      this.globalData.user.role = wx.getStorageSync("user.role");
-    }
+    console.log("onLaunch");
+    this.getCssStyle();
   },
   /***
    * 获取用户信息 
    */
   getLogin:function(){
     var that=this;
+    var host=this.globalData.host;
     wx.getUserInfo({
       success: function (res) {
+        console.log(res.code);
+        if (1==1) {
+          //发起网络请求
+          wx.request({
+            url: host + "user.do",
+            method: "post",
+            data: {
+              method: "getOpenid",
+              code: res.code
+            },
+            header: {
+              'content-type': 'application/x-www-form-urlencoded'
+            },
+            success: function (res) {
+              var openid = res.data.openid;
+              console.log(openid);
+              if (openid == "" || openid == null || openid == "null") {
+                wx.showModal({
+                  title: "系统提示",
+                  content: "读取用户信息失败，请重启程序",
+                  showCancel: false,
+                  confirmText: "确定"
+                })
+              } else {
+                that.setData({
+                  openid: openid
+                })
+                that.checkAuth();//发送审核核实请求
+              }
+            },
+            fail: function (res) {
+              wx.showModal({
+                title: "操作异常",
+                content: "请检查网络或重启程序,",
+                showCancel: false,
+                confirmText: "确定"
+              })
+            }
+          })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
         var userInfo = res.userInfo;
         that.globalData.user.uname= userInfo.nickName;
         that.globalData.user.uimg= userInfo.avatarUrl;
@@ -51,20 +96,55 @@ App({
         wx.setStorageSync("user.uimg", that.globalData.user.uimg);
         wx.setStorageSync("user.tel", that.globalData.user.tel);
         wx.setStorageSync("user.role", "customer");
-        wx.setStorageSync("ustatus", "pass");
+        wx.setStorageSync("ustatus", "");
       }
     });
   },
+  /**
+   * 检查服务器样式数据
+   */
+  getCssStyle:function(){
+    //发起网络请求
+    var that = this;
+    var host = this.globalData.host;
+    wx.request({
+      url: host + "styles.do",
+      method: "post",
+      data: {
+        method: "getStyle"
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        var styles = res.data[0];
+        var holiday=styles.holiday;
+        if (holiday!="" && holiday!=null) {
+           that.globalData.css=holiday;
+           that.globalData.AD=styles.holiday_ad;
+        }
+      },
+      fail: function (res) {
+        wx.showModal({
+          title: "操作异常",
+          content: "请检查网络或重启程序,",
+          showCancel: false,
+          confirmText: "确定"
+        })
+      }
+    })
+  },
+  //每个页面设置统一样式
   setCssStyle:function(){
     var bgColor = "";
     var fColor = "";
     switch (this.globalData.css) {
-      case "common": { bgColor = "#ffb53c"; fColor = "#ffffff"; break; }
       case "dw": { bgColor = "#ffffff"; fColor = "#000000"; break; }
       case "PLA": { bgColor = "#f3cf61"; fColor = "#ffffff"; break; }
       case "father": { bgColor = "#f3c720"; fColor = "#ffffff"; break; }
       case "moon": { bgColor = "#051a53"; fColor = "#ffffff"; break; }
       case "china": { bgColor = "#950a0d"; fColor = "#ffffff"; break; }
+      default: { bgColor = "#ffb53c"; fColor = "#ffffff"; break; }
     }
     wx.setNavigationBarColor({
       backgroundColor: bgColor,
